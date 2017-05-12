d31e6e5a05325af8faa83a905daa4cf5f1117631
dnair2s <deebul.nair@smail.inf.h-brs.de>
2017-05-12 16:15:03 +0200

    Porting to python3 used command 2to3

--- corenlp.py
+++ corenlp.py
@@ -104,7 +104,7 @@ def parse_parser_results(text):
             else:
                 split_entry = re.split("\(|, ", line[:-1])
                 if len(split_entry) == 3:
-                    rel, left, right = map(lambda x: remove_id(x), split_entry)
+                    rel, left, right = split_entry
                     sentence['dependencies'].append(tuple([rel,left,right]))
         
         elif state == STATE_COREFERENCE:
@@ -223,7 +223,7 @@ class StanfordCoreNLP(object):
             logger.debug("%s\n%s" % ('='*40, incoming))
         try:
             results = parse_parser_results(incoming)
-        except Exception, e:
+        except Exception as e:
             if VERBOSE: 
                 logger.debug(traceback.format_exc())
             raise e
